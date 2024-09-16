611d5a639c6f5f2fa81bacfd5d0302db39d64ea6
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2024-09-16 18:31:05 +0300

    update types and docstrings

--- src/resources/subscribers/dto/del.dto.ts
+++ src/resources/subscribers/dto/del.dto.ts
@@ -1,12 +1,16 @@
-// https://help.senler.ru/senler/dev/api/methods/podpischiki/udalenie-podpischika
+/** https://help.senler.ru/senler/dev/api/methods/podpischiki/udalenie-podpischika */
 
 export interface DelSubscriberFromSubscriptionGroupRequestParams {
-  // VKontakte user ID
-  // Example: 1
+  /**
+   * VKontakte user ID
+   * Examples: 1; [1, 2, 3]
+   */
   vk_user_id: number | number[];
 
-  // ID of the subscriber group (0 - full unsubscription from all groups)
-  // Example: 123
+  /**
+   * ID of the subscriber group (0 - full unsubscription from all groups)
+   * Example: 123
+   */
   subscription_id: number;
 }
 
--- src/resources/subscribers/dto/subscriber.dto.ts
+++ src/resources/subscribers/dto/subscriber.dto.ts
@@ -1,33 +1,40 @@
+/** https://help.senler.ru/senler/dev/api/methods/podpischiki/poluchenie-podpischikov#struktura-elementa-massiva-s-podpischikami-items */
+
 import { Subscription } from './subscriber.subscription.dto';
 import { UtmFilterParams } from './subscriber.utm.dto';
 import { Var } from './subscriber.var.dto';
 
-
-// https://help.senler.ru/senler/dev/api/methods/podpischiki/poluchenie-podpischikov#struktura-elementa-massiva-s-podpischikami-items
 export interface Subscriber {
-  // Date of the first subscription format: d.m.Y H:i:s
-  // Example: 27.11.2018 10:00:00
+  /**
+   * Date of the first subscription format: d.m.Y H:i:s
+   * Example: 27.11.2018 10:00:00
+   */
   date: string;
 
-  // VKontakte subscriber ID
-  // Example: 1
+  /**
+   * VKontakte subscriber ID
+   * Example: 1
+   */
   vk_user_id: number;
 
-  // If the user is blacklisted, the value is "1", otherwise "0"
+  /** If the user is blacklisted, the value is "1", otherwise "0" */
   ignore: 0 | 1;
 
   first_name: string;
   last_name: string;
 
-  // Link to the subscriber's image
-  // Example: https://sun6-6.userapi.com;
+  /**
+   * Link to the subscriber's image
+   * Example: https://sun6-6.userapi.com;
+   */
   photo: string;
 
-  // List of subscriptions to groups
+  /** List of subscriptions to groups */
   subscriptions: Subscription[];
 
-  // List of Senler tags and utm tags
+  /** List of Senler tags and utm tags */
   utms: UtmFilterParams[];
 
+  /** List of Senler vars */
   vars?: Var[];
 }
