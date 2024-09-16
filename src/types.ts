6227673b9b0d55c6a93f6bb7f50302c50294300c
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2024-09-16 13:03:05 +0300

    add more types for import

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
-// TODO: import more
-
 export { BaseResponse } from './core/response/base-response';
+
+// Subscribers
+export { GetSubscribersRequestParams, GetSubscribersResponse,} from './resources/subscribers/dto/get.dto';
+export { AddSubscribersInGroupRequestParams, AddSubscribersInGroupResponse, AddSubscribersInGroupUserOperationDetails,} from './resources/subscribers/dto/add.dto';
+export { DelSubscriberFromSubscriptionGroupRequestParams, DelSubscriberFromSubscriptionGroupResponse,} from './resources/subscribers/dto/del.dto';
+export { GetSubscriptionsCountStatisticResponse, GetSubscriptionsCountStatisticsRequestParams,} from './resources/subscribers/dto/statCount.dto';
+export { GetSubscriptionsStatisticResponse, GetSubscriptionsStatisticsRequestParams,} from './resources/subscribers/dto/statSubscribe.dto';
