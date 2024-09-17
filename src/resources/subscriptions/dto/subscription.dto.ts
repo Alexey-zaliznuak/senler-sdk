33469a4b0bdc414849ad86be8e63455b8056128b
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2024-09-17 11:02:41 +0300

    small updates

--- src/resources/subscriptions/dto/subscription.dto.ts
+++ src/resources/subscriptions/dto/subscription.dto.ts
@@ -1,6 +1,6 @@
-// // Group of subscribers
-// // https://help.senler.ru/senler/dev/api/methods/gruppy-podpischikov/poluchenie-spiska-grupp-podpischikov
-// export interface Subscription {
-//   subscription_id: number;
-//   name: string;
-// }
+// Group of subscribers
+// https://help.senler.ru/senler/dev/api/methods/gruppy-podpischikov/poluchenie-spiska-grupp-podpischikov
+export interface Subscription {
+  subscription_id: number;
+  name: string;
+}
